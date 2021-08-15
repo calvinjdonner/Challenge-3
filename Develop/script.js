dc12d0d2b7b53466fc353cd701fc106a99441001
Calvin Donner <calvinjdonner@gmail.com>
2021-08-14 23:16:11 -0600

    fixed readme

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var specials = "'@','%','+','/','!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'";
-var uppers = "'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'";
-var lowers = "'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'";
-var numbers = "'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'";
+var specials = ['@','%','+','/','!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
+var uppers = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowers = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var finalPassOptions = [];
 var password = [];
 
@@ -16,47 +16,54 @@ function writePassword() {
   var questionOptions = questions();
   console.log(questionOptions);
   console.log(questionOptions.length);
+  var passLength = questionOptions.length // *** can I set a var equal to the length of another variable
 
   if (questionOptions.lower === true ) {
-    (questionOptions.length - 1); // this function isn't working. How do I deduct 1 of each character from the list to ensure it's captured
+    passLength - 1; // *** this function isn't working. How do I deduct 1 of each character from the list to ensure it's captured
     finalPassOptions.push('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
-    password.push(finalPassOptions[Math.floor(Math.random() * finalPassOptions.length)]); // this function will select four random characters, but they aren't always part of the above group
+    password.push(lowers[Math.floor(Math.random() * lowers.length)]); 
   }
 
   if (questionOptions.upper === true ) {
-    (questionOptions.length - 1);
+    passLength - 1;
     finalPassOptions.push('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
-    password.push(finalPassOptions[Math.floor(Math.random() * finalPassOptions.length)]);
+    password.push(uppers[Math.floor(Math.random() * uppers.length)]);
   }
 
   if(questionOptions.special === true) {
-    (questionOptions.length - 1);
+    passLength - 1;
     finalPassOptions.push('@','%','+','/','!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.');
-    password.push(finalPassOptions[Math.floor(Math.random() * finalPassOptions.length)]);
+    password.push(specials[Math.floor(Math.random() * specials.length)]);
   }
 
   if(questionOptions.number === true) {
-    (questionOptions.length - 1);
+    passLength - 1;
     finalPassOptions.push('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
-    password.push(finalPassOptions[Math.floor(Math.random() * finalPassOptions.length)]);
+    password.push(numbers[Math.floor(Math.random() * numbers.length)]);
   }
 
   console.log(finalPassOptions);
-  console.log(questionOptions.length);
+  console.log(passLength);
   console.log(password);
 
-  for(let i = 0; i < questionOptions.length; i++){
+  for(let i = 0; i < passLength; i++){
     //password.push(finalPassOptions(Math.floor(Math.random())));
   }
   
-  password.join('')
-
-  //if (questionOptions.lower === true) { // push letter from lower array and/or run forloop logic over lower array}
+  password.join(''); // *** console is still printing commas despite join function. Need to fix ***
 }
 
-  //need to offer options for password criteria. criteria needs to include length (>=8, <=128)
+//need to offer options for password criteria. criteria needs to include length (>=8, <=128)
 function questions() {
   var length = window.parseInt(prompt("Please indicate password length between 8 and 128 characters"));
+
+  // *** is an if statement needed to ensure user selects between 8 and 128 characters ?
+    if(length.length >= 8 && length.length <= 128) {
+      alert("Confirmed! Password will be " + length.length + "characters long!")
+  }
+  else {
+    length;
+  };
 
   var upper = window.confirm("Would you like to include uppercase letters in your password?");
 
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Challenge-3
+# Random Password Generator 
+
+## Purpose
+Provide employees with a randomly generated password that meets certain criteria (upper and lowercase letters, numbers, and symbols). This will allow them to create a strong password that provides greater security.
+
+## Built With
+* HTML
+* CSS
+* JavaScript
+
+##Contributing
+Calvin Donner
+
+## 
+Website
+file:///C:/Users/calvi/Desktop/Challenge-3/Develop/index.html
