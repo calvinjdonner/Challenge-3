d0780cdabdd67acf2e75a96a9952716eb1f7881e
Calvin Donner <calvinjdonner@gmail.com>
2021-08-15 21:48:45 -0600

    added if statement to ensure <8, >128, and NaN weren't options

--- Develop/script.js
+++ Develop/script.js
@@ -6,20 +6,21 @@ var lowers = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var finalPassOptions = [];
 var password = [];
+var passLength = [];
 
 // Write password to the #password input
 function writePassword() {
   //var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = password.join('');
   var questionOptions = questions();
   console.log(questionOptions);
   console.log(questionOptions.length);
-  var passLength = questionOptions.length // *** can I set a var equal to the length of another variable
+  //var passLength = questionOptions.length // *** can I set a var equal to the length of another variable
 
   if (questionOptions.lower === true ) {
-    passLength - 1; // *** this function isn't working. How do I deduct 1 of each character from the list to ensure it's captured
+    questionOptions.length - 1; // *** this function isn't working. How do I deduct 1 of each character from the list to ensure it's captured
     finalPassOptions.push('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
     password.push(lowers[Math.floor(Math.random() * lowers.length)]); 
   }
@@ -43,27 +44,24 @@ function writePassword() {
   }
 
   console.log(finalPassOptions);
-  console.log(passLength);
-  console.log(password);
+  console.log(questionOptions.length);
+  console.log(password.join(''));
 
   for(let i = 0; i < passLength; i++){
     //password.push(finalPassOptions(Math.floor(Math.random())));
   }
   
-  password.join(''); // *** console is still printing commas despite join function. Need to fix ***
+  password.join('');
 }
 
 //need to offer options for password criteria. criteria needs to include length (>=8, <=128)
 function questions() {
   var length = window.parseInt(prompt("Please indicate password length between 8 and 128 characters"));
-
-  // *** is an if statement needed to ensure user selects between 8 and 128 characters ?
-    if(length.length >= 8 && length.length <= 128) {
-      alert("Confirmed! Password will be " + length.length + "characters long!")
-  }
-  else {
-    length;
-  };
+     // *** is an if statement needed to ensure user selects between 8 and 128 characters ?
+    if(length < 8 || length > 128 || isNaN(length) || length === null) {
+      window.alert("You need to provide a valid answer. Please try again.");
+      return questions();
+    }
 
   var upper = window.confirm("Would you like to include uppercase letters in your password?");
 
